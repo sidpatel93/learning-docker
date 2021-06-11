468f3f055b97dac78035eb2d4d7cb97541dd17db
Sid Patel <sid.auburn@gmail.com>
2021-06-11 14:31:53 -0500

    created multi container app using docker compos - bug in the byte literal

--- hello.py
+++ hello.py
@@ -1,10 +1,29 @@
 from flask import Flask
+from flask import render_template
+import redis
 
 app = Flask(__name__)
+r = redis.Redis(host = 'redis-server', port=6379)
+r.set('visit',0)
+test = 0
+
+def visitcount():
+    visit = int.from_bytes(r.get('visit'),"little") - 48
+    r.set('visit', visit+1)
+    return visit
+
+def bytes_to_int(bytes):
+    result = 0
+
+    for b in bytes:
+        result = result * 256 + int(b)
+
+    return result    
 
 @app.route("/")
 def hello_world():
-    return "<p> Hello, World! from flask </p>"
+    return render_template('hello.html', test = visitcount())
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5500)
+
